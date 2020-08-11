bb47f548d35f2c76cfbaaa1b559db7308b3c9c6c
cristianvaldes24 <58448827+cristianvaldes24@users.noreply.github.com>
2020-08-11 00:19:44 -0600

    segundo avance

---
+++ pinterest_backend/api/usuarios.php
@@ -0,0 +1,35 @@
+<?php
+
+    header("Content-Type: aplication/json");
+    include_once("../clases/class-usuario.php");
+
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'POST':
+            $_POST = json_decode(file_get_contents('php://input'),true);
+            $usuario = new Usuario($_POST["correo"], $_POST["contrasenia"], $_POST["edad"]);
+            $usuario->guardarUsuario();
+            $resultado["mensaje"] = "guardar el usuario con la informacion: ".json_encode($_POST);
+            echo json_encode($resultado);
+        break;
+        case 'GET':
+            if(isset($_GET['id'])){
+                Usuario::obtenerUsuario($_GET['id']);
+            }
+            else{
+                Usuario::obtenerUsuarios();
+            } 
+        break;
+        case 'PUT':
+            $_PUT = json_decode(file_get_contents('php://input'),true);
+            $usuario = new Usuario($_PUT['correo'], $_PUT['contrasenia'], $_PUT['edad']);
+            $usuario->actualizarUsuario($_GET['id']);
+            $resultado["mensaje"] = "Actualizar el usuario con el id: ". $_GET['id']
+                                    .", Se actualizara la informacion: ". json_encode($_PUT);
+            echo json_encode($resultado);
+        break;
+        case 'DELETE':
+            $resultado["mensaje"] = "Eliminar el usuario con el id: ". $_GET['id'];
+            echo json_encode($resultado);
+        break;
+    }
+?>
---
+++ pinterest_backend/clases/class-usuario.php
@@ -0,0 +1,94 @@
+<?php
+
+class Usuario{
+
+    private $correo;
+    private $contrasenia;
+    private $edad;
+
+    public function __construct(
+        $correo,
+        $contrasenia,
+        $edad
+    ){
+        $this->correo = $correo;
+        $this->contrasenia = $contrasenia;
+        $this->edad = $edad;
+    }
+
+    public function setCorreo($correo){
+        $this->correo = correo;
+        return $this->correo;
+    }
+
+    public function getCorreo(){
+        return $this->correo;
+    }
+
+    public function setContrasenia($contrasenia){
+        $this->contrasenia = $contrasenia;
+        return $this;
+    }
+
+    public function getcontrasenia(){
+        return $this->contrasenia;
+    }
+
+    public function setEdad($edad){
+        $this->edad = $edad;
+        return $this;
+    }
+
+    public function getEdad(){
+        return $this->edad;
+    }
+
+    public function __toString(){
+        return $this->correo ." ".$this->contrasenia." ".$this->edad;
+    }
+
+//CRUD
+    public function guardarUsuario(){
+        $contenidoArchivo = file_get_contents("../data/usuarios.json"); 
+        $usuarios = json_decode($contenidoArchivo, true);
+        $usuarios[] = array (
+                    "correo"=> $this->correo,
+                    "contrasenia"=> $this->contrasenia,
+                    "edad"=> $this->edad
+        );
+        $archivo = fopen("../data/usuarios.json","w");
+        fwrite($archivo,json_encode($usuarios));
+        fclose($archivo);
+    }
+    
+    public static function obtenerUsuario($indice){
+        $contenidoArchivo = file_get_contents("../data/usuarios.json"); 
+        $usuarios = json_decode($contenidoArchivo, true);
+        echo json_encode($usuarios[$indice]);
+    }
+    
+    public static function obtenerUsuarios(){
+        $contenidoArchivo = file_get_contents("../data/usuarios.json"); 
+        echo $contenidoArchivo;
+    }
+
+    public function actualizarUsuario($indice){
+        $contenidoArchivo = file_get_contents("../data/usuarios.json"); 
+        $usuarios = json_decode($contenidoArchivo, true);
+        $usuario = $usuarios[$indice];
+        $usuario = array(
+                'correo'=> $this->correo,
+                'contrasenia'=> $this->contrasenia,
+                'edad'=> $this->edad
+        )
+        $usuarios[$indice] = $usuario;
+        $archivo = fopen('../data/usuarios.json','w');
+        fwrite($archivo, json_encode($usuarios));
+        fclose($archivo);
+    }
+    public function eliminarUsuario(){
+
+    }
+}
+
+?>
